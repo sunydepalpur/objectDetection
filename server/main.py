aefcce9f1b701e3b9981aea4db027061f46de1a4
Somesh Choudhary -X (somechou - ZENSAR TECHNOLOGIES LIMITED at Cisco) <somechou@partnet.cisco.com>
2020-02-03 13:25:16 +0530

    package change

--- server/main.py
+++ server/main.py
@@ -3,23 +3,25 @@ import json
 import logging
 
 from flask import Flask
-from flask_sockets import Sockets
+#from flask_sockets import Sockets
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-sockets = Sockets(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 HTTP_SERVER_PORT = 5000
 
-@sockets.route('/media')
+@socketio.on('/media')
 def echo(ws):
-    app.logger.info("Connection accepted")
+    print("Connection accepted")
     # A lot of messages will be sent rapidly. We'll stop showing after the first one.
     has_seen_media = False
     message_count = 0
     while not ws.closed:
         message = ws.receive()
         if message is None:
-            app.logger.info("No message received...")
+            print("No message received...")
             continue
 
         # Messages are a JSON encoded string
@@ -27,31 +29,26 @@ def echo(ws):
 
         # Using the event type you can determine what type of message you are receiving
         if data['event'] == "connected":
-            app.logger.info("Connected Message received: {}".format(message))
+            print("Connected Message received: {}".format(message))
         if data['event'] == "start":
-            app.logger.info("Start Message received: {}".format(message))
+            print("Start Message received: {}".format(message))
         if data['event'] == "media":
             if not has_seen_media:
-                app.logger.info("Media message: {}".format(message))
+                print("Media message: {}".format(message))
                 payload = data['media']['payload']
-                app.logger.info("Payload is: {}".format(payload))
+                print("Payload is: {}".format(payload))
                 chunk = base64.b64decode(payload)
-                app.logger.info("That's {} bytes".format(len(chunk)))
-                app.logger.info("Additional media messages from WebSocket are being suppressed....")
+                print("That's {} bytes".format(len(chunk)))
+                print("Additional media messages from WebSocket are being suppressed....")
                 has_seen_media = True
         if data['event'] == "stop":
-            app.logger.info("Stop Message received: {}".format(message))
+            print("Stop Message received: {}".format(message))
             break
         message_count += 1
 
-    app.logger.info("Connection closed. Received a total of {} messages".format(message_count))
+    print("Connection closed. Received a total of {} messages".format(message_count))
 
 
-if __name__ == '__main__':
-    app.logger.setLevel(logging.DEBUG)
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-
-    server = pywsgi.WSGIServer(('', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
-    print("Server listening on: http://localhost:" + str(HTTP_SERVER_PORT))
-    server.serve_forever()
+if __name__ == '__main__':   
+    socketio.run(app)
+    print("Server listening on: http://localhost:" )
